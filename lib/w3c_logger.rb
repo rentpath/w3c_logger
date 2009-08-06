50da2654138220cad85a5f49aec457909e81d35c
Jason Noble <perlwizard@gmail.com>
2009-08-06 15:33:35 -0400

    Updated to work with SyslogLogger

--- lib/w3c_logger.rb
+++ lib/w3c_logger.rb
@@ -2,15 +2,23 @@ module ActionController
   class Base
     class << self
       def w3c_logger
-        @w3c_logger ||= Logger.new("#{RAILS_ROOT}/log/w3c.log")
+        @w3c_logger ||= case RAILS_DEFAULT_LOGGER.class.to_s
+        when 'SyslogLogger'
+          # Syslog is in use, use it to log messages
+          RAILS_DEFAULT_LOGGER
+        else
+          # Regular log files, create a seperate log file
+          Logger.new("#{RAILS_ROOT}/log/w3c.log")
+        end
+        @w3c_logger
       end
     end
-    
+
     # Place logger reference into instance variable for performance improvement
     def w3c_logger
       @w3c_logger ||= self.class.w3c_logger
     end
-    
+
     def perform_action_with_new_log
       ms = [Benchmark.ms { perform_action_without_new_log }, 0.01].max
       line = []
@@ -26,12 +34,12 @@ module ActionController
       line << request.cookies.length                          || "-"
       w3c_logger.info line.join("\t")
     end
-    
+
     alias_method :perform_action_without_new_log, :perform_action
     alias_method :perform_action, :perform_action_with_new_log
 
     private :perform_action
-    
+
     def quotify(field)
       "\"#{field}\""
     end
